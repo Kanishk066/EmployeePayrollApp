cba0c41cd9fea5f3dfe318b87d9a756f35bf8ddd
Kanishk066 <kanishksinghal06@gmail.com>
2023-06-10 10:57:32 +0530

    Added the code to Employee Payroll Class with New Attributes

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -80,4 +80,13 @@ class EmployeePayrollData {
       set Notes(notes) {
         this._notes = notes;
       }
+      
+      toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined":
+        this.startDate. toLocaleDateString("en-US", options);
+        return "id=" + this.id +", name='" + this.name +", gender='" + this.gender +
+        ", profilePic='" + this.profilePic + ", department=" + this.department +
+         ",salary=" + this.salary +", startDate=" + empDate + ", note="+ this.note; 
+      }
     }
