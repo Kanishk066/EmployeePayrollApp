061f6031436eee64a0823f0473a660a6996c0717
Kanishk066 <kanishksinghal06@gmail.com>
2023-06-12 15:57:14 +0530

    Added the code Display Employee Details from Local Storage

--- HomePage.js
+++ HomePage.js
@@ -1,10 +1,21 @@
-window. addEventListener('DOMContentLoaded', (event) => {
+let empPayrollList;
+window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
      createInnerHtml();
+     localStorage.removeItem('editEmp');
  });
+
+ const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? 
+    JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+ }
+
  const createInnerHtml = () => {
-    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" + "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" + 
+    "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    if(empPayrollList.length == 0) return;
     let innerHtml = `${headerHtml}`;
-    let empPayrollList = creteEmployeePayrollJSON();
     for (const empPayrollData of empPayrollList) {
      innerHtml = `${innerHtml}
     <tr>
@@ -13,7 +24,7 @@ window. addEventListener('DOMContentLoaded', (event) => {
          <td>${empPayrollData._gender}</td>
          <td>${getDeptHtml(empPayrollData._department)}</td>
          <td>${empPayrollData._salary}</td>
-         <td>${empPayrollData._startDate}</td>
+         <td>${stringifyDate(empPayrollData._startDate)}</td>
          <td>
              <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="delete-black-18dp.svg">
              <img id="${empPayrollData._id}" alt="edit" onclick="update(this)" src="create-black-18dp.svg">
